2da080f7dee617d35ed228bca486f82a6284a129
Adam Michals <michalsa@purdue.edu>
2018-04-19 20:04:09 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -8,4 +8,4 @@ for elem in sys.argv[1:]:
 mean = mean / float(len(sys.argv) - 1)
 print("Thoust's mean be:", mean)
 word = input("insert a descriptor: ")
-print("stev is a", word)
+print("steev is a", word)
